2da473707fa9886337893a766acb74772470d37e
rauljmz <rauljmz@gmail.com>
2013-06-26 08:05:54 +0100

    parameter processing

--- Sitecore.Pump/Program.cs
+++ Sitecore.Pump/Program.cs
@@ -10,6 +10,73 @@ namespace Sitecore.Pump
     {
         static void Main(string[] args)
         {
+            var connectionStrings = new ConnectionStrings();
+           
+            for (var index = 0; index < args.Length; index++)
+            {
+                var arg = args[index];
+                if (arg.Equals("--config", StringComparison.InvariantCultureIgnoreCase))
+                {                    
+                    connectionStrings = new ConnectionStrings(args[index+1]);
+                    index++;
+                }               
+            }
+
+            var serializationManager = new SerializationManager(connectionStrings);
+            if (args[0].Equals("serialize", StringComparison.InvariantCultureIgnoreCase))
+            {
+                serializationManager.Serialize();
+            }
+            else
+            {
+                serializationManager.Deserialize();
+            }
+
+
+        }
+
+        static void DisplayUsage()
+        {
+            Console.WriteLine("Usage: Sitecore.Pump serialize|deserialize [databasename] [databasename] --config configfile");
+        }
+    }
+    public class ConnectionStrings
+    {
+        /// <summary>
+        /// Creates conn strings from a standard connectionstring file
+        /// </summary>
+        /// <param name="s"></param>
+        public ConnectionStrings(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates conn strings from the app.config
+        /// </summary>
+        public ConnectionStrings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SerializationManager
+    {
+        private ConnectionStrings _connectionStrings;
+
+        public SerializationManager(ConnectionStrings connectionStrings)
+        {
+            _connectionStrings = connectionStrings;
+        }
+
+        public void Serialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deserialize()
+        {
+            throw new NotImplementedException();
         }
     }
 }
